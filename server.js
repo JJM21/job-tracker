571f2331ad28f9802987e8a52a4dac18f5d03a34
Joshua <josh@cadf.co.za>
2026-02-26 08:44:45 +0200

    Fix port for Render

--- server.js
+++ server.js
@@ -264,8 +264,8 @@ console.log("User connected");
 // START SERVER
 // ============================
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-console.log("Server running on port " + PORT);
+  console.log("Server running on port " + PORT);
 });
